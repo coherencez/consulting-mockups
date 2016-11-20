776deb1d7fb07acc27b77e7ecdfb0f8b5ccb4766
Thomas Swann <tkswann@mac.com>
2016-11-20 11:01:28 -0600

    dynamic boxes, as well as exponential height growth on click

--- pug-example/public/lib/index.js
+++ pug-example/public/lib/index.js
@@ -1,4 +1,8 @@
 'use strict'
+const $outputDivForBoxes = $('#boxes')
+for (let i = 0; i < 100; i++) {
+  $outputDivForBoxes.append($('<div class="box"></div>'))
+}
 
 const getRandomHexColor = () => `#${Math.random().toString(16).slice(2,8)}`
 
@@ -9,13 +13,15 @@ const heightNum = Number(height.slice(0,2))
 const widthNum = Number(width.slice(0,2))
 let bool = true
 $button.on('click', () => {
-  if (bool) {
-    $button.css('height', `${heightNum + 100}px`)
-    $button.css('width', `${widthNum + 100}px`)
-  } else {
-    $button.css('height', height)
-    $button.css('width', width)
-  }
-  $('body').css('background-color', getRandomHexColor())
+
+  // if (bool) {
+  //   $button.css('height', `${heightNum + 100}px`)
+  //   $button.css('width', `${widthNum + 100}px`)
+  // } else {
+  //   $button.css('height', height)
+  //   $button.css('width', width)
+  // }
+  $('.box').css('height', (index, style) => index * 10)
+  $('body').css({backgroundColor: getRandomHexColor()})
   bool = !bool
 })
